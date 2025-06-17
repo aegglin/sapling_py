e319c9e77d0229ac2e3ab2e09405b30aaa939c13
Aiden Egglin <uefadude@gmail.com>
2025-06-17 18:44:51 -0400

    Add tiles and move Beetle files

--- beetle.py
+++ beetle.py
@@ -1,33 +1,35 @@
 import pygame
+
+import constants
 import utils
 from direction import Direction
-
 from gameobject import GameObject
+
 
 class Beetle(GameObject):
 
     def __init__(self, x, y, speed):
         GameObject.__init__(self, x, y, speed)
         # images
-        self.down1 = utils.load_image("assets", "BeetleDown1.png")
-        self.down1 = pygame.transform.scale(self.down1, (48, 48))
-        self.down2 = utils.load_image("assets", "BeetleDown2.png")
-        self.down2 = pygame.transform.scale(self.down2, (48, 48))
+        self.down1 = utils.load_image("assets/beetle", "BeetleDown1.png")
+        self.down1 = pygame.transform.scale(self.down1, (constants.TILE_SIZE, constants.TILE_SIZE))
+        self.down2 = utils.load_image("assets/beetle", "BeetleDown2.png")
+        self.down2 = pygame.transform.scale(self.down2, (constants.TILE_SIZE, constants.TILE_SIZE))
 
-        self.up1 = utils.load_image("assets", "BeetleUp1.png")
-        self.up1 = pygame.transform.scale(self.up1, (48, 48))
-        self.up2 = utils.load_image("assets", "BeetleUp2.png")
-        self.up2 = pygame.transform.scale(self.up2, (48, 48))
+        self.up1 = utils.load_image("assets/beetle", "BeetleUp1.png")
+        self.up1 = pygame.transform.scale(self.up1, (constants.TILE_SIZE, constants.TILE_SIZE))
+        self.up2 = utils.load_image("assets/beetle", "BeetleUp2.png")
+        self.up2 = pygame.transform.scale(self.up2, (constants.TILE_SIZE, constants.TILE_SIZE))
 
-        self.left1 = utils.load_image("assets", "BeetleLeft1.png")
-        self.left1 = pygame.transform.scale(self.left1, (48, 48))
-        self.left2 = utils.load_image("assets", "BeetleLeft2.png")
-        self.left2 = pygame.transform.scale(self.left2, (48, 48))
+        self.left1 = utils.load_image("assets/beetle", "BeetleLeft1.png")
+        self.left1 = pygame.transform.scale(self.left1, (constants.TILE_SIZE, constants.TILE_SIZE))
+        self.left2 = utils.load_image("assets/beetle", "BeetleLeft2.png")
+        self.left2 = pygame.transform.scale(self.left2, (constants.TILE_SIZE, constants.TILE_SIZE))
 
-        self.right1 = utils.load_image("assets", "BeetleRight1.png")
-        self.right1 = pygame.transform.scale(self.right1, (48, 48))
-        self.right2 = utils.load_image("assets", "BeetleRight2.png")
-        self.right2 = pygame.transform.scale(self.right2, (48, 48))
+        self.right1 = utils.load_image("assets/beetle", "BeetleRight1.png")
+        self.right1 = pygame.transform.scale(self.right1, (constants.TILE_SIZE, constants.TILE_SIZE))
+        self.right2 = utils.load_image("assets/beetle", "BeetleRight2.png")
+        self.right2 = pygame.transform.scale(self.right2, (constants.TILE_SIZE, constants.TILE_SIZE))
 
         self.direction = Direction.DOWN
         self.curr_sprite = self.down1
---
+++ tile.py
@@ -0,0 +1,5 @@
+import utils
+
+class Tile:
+    def __init__(self, image):
+        self.image = utils.load_image(image)
